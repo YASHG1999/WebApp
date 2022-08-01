9baa7a1b2efae5e9e03418d91e635050d15d48cb
Abhishek Dogra <ab@Abhisheks-MacBook-Pro.local>
2022-08-01 12:38:58 +0530

    initial commit

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+}
