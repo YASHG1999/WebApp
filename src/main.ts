4dc957eb0498e4f5abe357d4462c74f0174b8b75
Mohit Soni <mohit.soni@bigsteptech.com>
2022-09-16 16:16:22 +0530

    validation dto

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('auth');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Auth Service')
     .setDescription('Service for user authentication')
